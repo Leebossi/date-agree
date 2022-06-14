51ba8fc19ebdf6a028b6ea710228f128e77922c7
Leevi Ossi <leevi.ossi@hotmail.com>
2022-06-14 16:28:38 +0300

    basic calendar with buttons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,31 @@
+import {
+  add,
+  endOfMonth,
+  endOfWeek,
+  format,
+  getDay,
+  parse,
+  startOfToday,
+} from "date-fns";
+import { eachDayOfInterval } from "date-fns/esm";
 import React, { useState } from "react";
-import DatePicker from "./components/DatePicker";
 
 const App = () => {
+  const today = startOfToday();
   const [createClicked, setCreateClicked] = useState(false);
+  const [currentMonth, setCurrentMonth] = useState(format(today, "MMM-yyyy"));
+  const firstDayCurrentMonth = parse(currentMonth, "MMM-yyyy", new Date());
+
+  const newDays = eachDayOfInterval({
+    start: firstDayCurrentMonth,
+    end: endOfWeek(endOfMonth(firstDayCurrentMonth)),
+  });
+  console.log(newDays);
+
+  const nextMonth = () => {
+    const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+    setCurrentMonth(format(firstDayNextMonth, "MMM-yyyy"));
+  };
 
   return (
     <div className="app">
@@ -14,10 +37,50 @@ const App = () => {
           </button>
         </div>
       ) : (
-        <></>
+        <div className="calendar">
+          <div className="month-indicator">
+            <h2>{format(firstDayCurrentMonth, "MMMM yyyy")}</h2>
+            <button>p</button>
+            <button onClick={nextMonth}>n</button>
+          </div>
+          <div className="day-of-week">
+            <div>Mo</div>
+            <div>Tu</div>
+            <div>We</div>
+            <div>Th</div>
+            <div>Fr</div>
+            <div>Sa</div>
+            <div>Su</div>
+          </div>
+          <div className="date-grid">
+            {newDays.map((day, dayIndx) => (
+              <div
+                key={day.toString()}
+                className={`${dayIndx === 0 ? colStartClasses[getDay(day)] : ''}`}
+              >
+                <button type="button">
+                  <time dateTime={format(day, "yyyy-MM-dd")}>
+                    {format(day, "d")}
+                  </time>
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );
 };
+
+const colStartClasses = [
+  "",
+  "col-start-1",
+  "col-start-2",
+  "col-start-3",
+  "col-start-4",
+  "col-start-5",
+  "col-start-6",
+  "col-start-7",
+];
 
 export default App;
